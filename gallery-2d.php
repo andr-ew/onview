1b1b37c06d72456f6a3813fb9f5b36e343daba3d
andr-ew <andrewcshike@gmail.com>
2020-12-09 10:05:02 -0600

    gallery-2d.php

---
+++ gallery-2d.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Gallery 2D */ ?>
+
+<?php get_header(); ?>
+<body class="gallery-2d" style="background-image: url('<?php echo get_template_directory_uri() . "/img/bg.svg"; ?>')">
+	<header>
+		<div>
+			<p class="button back" id="back"><span class="left">→</span> back</p>
+		</div>
+		<div>
+			<h1 class="title" id="title"><?php echo get_the_title(); ?></h1>
+		</div>
+		<div class="title main">
+	    	<h1>SAIC On View</h1>
+	    </div>
+	</header>
+<?php get_footer(); ?>
